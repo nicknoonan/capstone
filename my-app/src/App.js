28ab7a3f42f39170eda74c46a40ee05ac852f999
zzMountainManzz <villemagnejp@appstate.edu>
2021-09-22 21:38:57 -0400

    My work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,24 +12,16 @@ import { Route, Link } from "react-router-dom";
 // Importing the NavBar for the navigation bar
 import NavBar from './NavBar';
 
-import Footer from "./Footer";
-
 // Included home and about in App's div
 function App() {
   return (
         
-      <div className="page-container">
-      <div className="content-wrap">
+      <div className="App">
       
-        
         <NavBar />
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
-
-      </div>
-      
-     
-    <Footer />
+  
     </div>
   );
 }
