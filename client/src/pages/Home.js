9e850a678b8049a8d816db1bc747bc70164dfd58
zzMountainManzz <villemagnejp@appstate.edu>
2021-10-03 13:27:15 -0400

    Commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,8 +4,17 @@ function Home() {
   return (
     <div>
       <h1>Welcome to Boone Houseing Help</h1>
+      
+      {/* picture */}
+      
+      {/* Browse moduel Moduel */}
+      
+      {/* Moduel 1 */}
+      
+      {/* Moduel 1 */}
     </div>
   );
 }
+
 
 export default Home;
