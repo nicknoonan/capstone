42c58c3e89fb7f8df6b4dbe85527449475f65672
nicknoonan <47186822+nicknoonan@users.noreply.github.com>
2021-11-10 20:28:20 -0500

    Delete secrets.js

--- server/secrets.js
+++
@@ -1,7 +0,0 @@
-const DB_USER_NAME = 'nickdev';
-const DB_PASSWORD = 'gimmeshelter';
-const DB_URL = 'mongodb+srv://' + DB_USER_NAME + ':' + DB_PASSWORD + '@boonehousinghelp.qcefq.mongodb.net/dev_boonehousinghelp?retryWrites=true&w=majority';
-const JWT_SECRET = 'nickjwtsecret';
-const EMAIL = 'boonehousinghelp@gmail.com'
-const EMAIL_PASSWORD = 'parry1234?'
-module.exports = { DB_URL, DB_USER_NAME, DB_PASSWORD, JWT_SECRET, EMAIL, EMAIL_PASSWORD };
