0d41e14c2ed23a324c8b01f5326c405555d25001
nicknoonan <47186822+nicknoonan@users.noreply.github.com>
2021-11-17 21:27:25 -0500

    new build

--- client/src/util.js
+++ client/src/util.js
@@ -2,9 +2,9 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 function get_domain() {
-  //return "http://boonehousinghelp.com";
+  return "http://boonehousinghelp.com";
   //return "http://localhost:8080";
-  return "http://localhost:3000";
+  //return "http://localhost:3000";
 }
 
 export { get_domain };
