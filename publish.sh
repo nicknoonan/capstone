5f92ffc57f9a463e2a011c1e2abb11fc62f1ea2c
nicknoonan <47186822+nicknoonan@users.noreply.github.com>
2021-11-19 18:16:16 -0500

    a

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
-BHH_DOMAIN_NAME=boonehousinghelp.com
-BHH_PORT=80
+#first stop boonehousing pm2 process
+pm2 stop boonehousinghelp
 DO_PULL=y
 DO_BUILD=y
-
+DO_LOG=n
 #handle any arguements passed
 args_index=0
 args_array=( "$@" )
@@ -16,17 +16,15 @@ do
   elif [ $arg = "--no-pull" ]  
   then
     DO_PULL=n
-  elif [ $arg = "--port" ]
+  #elif [ $arg = "--port" ]
+  #then
+  #  BHH_PORT=${args_array[$((args_index + 1))]}
+  elif [ $arg = "--log" ]
   then
-    BHH_PORT=${args_array[$((args_index + 1))]}
+    DO_LOG=y
   fi
 args_index=$((args_index + 1))
 done
-
-
-export BHH_DOMAIN_NAME
-export BHH_PORT
-
 #pull fresh code from git repo
 if [ $DO_PULL = "y" ]
 then
@@ -69,17 +67,21 @@ then
     exit $code
   fi
 else
-  echo "***************************************************"
-  echo "------------------SKIPPING--BUILD------------------"
-  echo "***************************************************"
+  echo "**************************************************"
+  echo "------------------SKIPPING--BUILD-----------------"
+  echo "**************************************************"
 fi
 #start up the pm2 process
-pm2 start boonehousinghelp
+pm2 start server/index.js -i 1 --name boonehousinghelp
 #check that the pm2 process started successfully
 if [ $? -eq 0 ]
 then
   echo "PORT : $BHH_PORT"
   echo "DOMAIN : $BHH_DOMAIN_NAME"
+  if [ $DO_LOG = "y" ]
+  then
+    pm2 log
+  fi
 else
   echo "***************************************************"
   echo "--------------------PM2--FAILED--------------------"
