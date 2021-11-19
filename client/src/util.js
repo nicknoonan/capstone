a5feedf0584befce09c1ac645893fc280eaa7dc0
nicknoonan <47186822+nicknoonan@users.noreply.github.com>
2021-11-19 15:58:14 -0500

    testing publish

--- client/src/util.js
+++ client/src/util.js
@@ -2,7 +2,7 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 function get_domain() {
-  return "http://boonehousinghelp.com";
+  return process.env.BHH_DOMAIN_NAME;
   //return "http://localhost:8080";
   //return "http://localhost:3000";
 }
